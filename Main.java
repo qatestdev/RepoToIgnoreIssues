de0e1b1363c3c005fd9af29d4fb62e32723e9caa
devcodacytestuser <devcodacytestuser@codacy.com>
2024-08-29 15:24:27 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    System.out.println("");
+  }
+}
